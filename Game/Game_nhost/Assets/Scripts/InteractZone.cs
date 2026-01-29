d46d27d5093e782e0496212267746ad5342961ae
haki <hrammixaxit@gmail.com>
2026-01-29 21:12:53 +0300

    feat(interaction): add interactable components (base setup)

---
+++ Game/Game_nhost/Assets/Scripts/InteractZone.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class InteractZone : MonoBehaviour
+{
+    [SerializeField] private MonoBehaviour interactableBehaviour;
+
+    public IInteractable Interactable => interactableBehaviour as IInteractable;
+
+    private void Reset()
+    {
+        var col = GetComponent<Collider>();
+        col.isTrigger = true;
+
+        // Автопоиск интерактива на родителе
+        interactableBehaviour = GetComponentInParent<MonoBehaviour>();
+    }
+}
